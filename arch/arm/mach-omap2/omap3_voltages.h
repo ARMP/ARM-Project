3a6559eb27bd7f864a0b88038a17a2a67202c8e4
Nicholas <nicholas.npcc@gmail.com>
2012-11-23 17:07:20 +0800

    Update OPP Values and include various voltage tweaks including 150MHz Removal before release due to kernel panics

--- arch/arm/mach-omap2/omap3_voltages.h
+++ arch/arm/mach-omap2/omap3_voltages.h
@@ -2,12 +2,17 @@
  * for MPU and CORE voltages
  */
 
-#define OMAP3630_VDD_MPU_OPP50_UV               1000000
-#define OMAP3630_VDD_MPU_OPP200_UV              900000
+#define OMAP3630_VDD_MPU_OPP50_UV               975000
+#define OMAP3630_VDD_MPU_OPP70_UV               1050000
+#define OMAP3630_VDD_MPU_OPP85_UV               1095000
+#define OMAP3630_VDD_MPU_OPP65_UV               925000
 #define OMAP3630_VDD_MPU_OPP100_UV              1150000
 #define OMAP3630_VDD_MPU_OPP120_UV              1275000
 #define OMAP3630_VDD_MPU_OPP900_UV              1350000
 #define OMAP3630_VDD_MPU_OPP1G_UV               1375000
+#define OMAP3630_VDD_MPU_OPP11G_UV              1400000
+#define OMAP3630_VDD_MPU_OPP12G_UV              1425000
+#define OMAP3630_VDD_MPU_OPP12G_UV              1475000
 
 #define OMAP3630_VDD_CORE_OPP50_UV              1000000
 #define OMAP3630_VDD_CORE_OPP100_UV             1200000
