2220e4d58bc2dabe892554fd120ba1ae1dbdb91c
Nicholas <nicholas.npcc@gmail.com>
2012-11-23 13:09:33 +0800

    Add new OPP for 900MHz

--- arch/arm/mach-omap2/omap3_voltages.h
+++ arch/arm/mach-omap2/omap3_voltages.h
@@ -3,8 +3,10 @@
  */
 
 #define OMAP3630_VDD_MPU_OPP50_UV               1000000
+#define OMAP3630_VDD_MPU_OPP200_UV              900000
 #define OMAP3630_VDD_MPU_OPP100_UV              1150000
-#define OMAP3630_VDD_MPU_OPP120_UV              1300000
+#define OMAP3630_VDD_MPU_OPP120_UV              1275000
+#define OMAP3630_VDD_MPU_OPP900_UV              1350000
 #define OMAP3630_VDD_MPU_OPP1G_UV               1375000
 
 #define OMAP3630_VDD_CORE_OPP50_UV              1000000
