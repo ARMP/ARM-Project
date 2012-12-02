0de4040dd834744b8777b14676a144b30b4cdc59
nicholaschw <nicholas.npcc@gmail.com>
2012-12-02 22:31:45 +0000

    Remove all Overclock Values and voltages

--- arch/arm/mach-omap2/omap3_voltages.h
+++ arch/arm/mach-omap2/omap3_voltages.h
@@ -3,14 +3,9 @@
  */
 
 #define OMAP3630_VDD_MPU_OPP50_UV               1020000
-#define OMAP3630_VDD_MPU_OPP70_UV               1050000
-#define OMAP3630_VDD_MPU_OPP85_UV               1095000
-#define OMAP3630_VDD_MPU_OPP65_UV               1000000
-#define OMAP3630_VDD_MPU_OPP120_UV              1275000
+#define OMAP3630_VDD_MPU_OPP120_UV              1175000
 #define OMAP3630_VDD_MPU_OPP900_UV              1350000
 #define OMAP3630_VDD_MPU_OPP1G_UV               1375000
-#define OMAP3630_VDD_MPU_OPP12G_UV              1425000
-#define OMAP3630_VDD_MPU_OPP13G_UV              1475000
 
 #define OMAP3630_VDD_CORE_OPP50_UV              1000000
 #define OMAP3630_VDD_CORE_OPP100_UV             1200000
