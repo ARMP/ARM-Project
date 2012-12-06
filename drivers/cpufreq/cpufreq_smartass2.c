88a341d547d2de2b7f57844bbc2bfe8e6f2efcc1
Nicholas <nicholas.npcc@gmail.com>
2012-12-06 15:48:39 +0800

    Cleanup Attempts and failures

--- drivers/cpufreq/cpufreq_smartass2.c
+++ drivers/cpufreq/cpufreq_smartass2.c
@@ -53,7 +53,7 @@ static unsigned int awake_ideal_freq;
  * that practically when sleep_ideal_freq==0 the awake_ideal_freq is used
  * also when suspended).
  */
-#define DEFAULT_SLEEP_IDEAL_FREQ 300000
+#define DEFAULT_SLEEP_IDEAL_FREQ 100000
 static unsigned int sleep_ideal_freq;
 
 /*
@@ -67,7 +67,7 @@ static unsigned int ramp_up_step;
  * Freqeuncy delta when ramping down.
  * zero disables and will calculate ramp down according to load heuristic.
  */
-#define DEFAULT_RAMP_DOWN_STEP 100000
+#define DEFAULT_RAMP_DOWN_STEP 0
 static unsigned int ramp_down_step;
 
 /*
@@ -91,14 +91,14 @@ static unsigned long up_rate_us;
 /*
  * The minimum amount of time to spend at a frequency before we can ramp down.
  */
-#define DEFAULT_DOWN_RATE_US 99000;
+#define DEFAULT_DOWN_RATE_US 66000;
 static unsigned long down_rate_us;
 
 /*
  * The frequency to set when waking up from sleep.
  * When sleep_ideal_freq=0 this will have no effect.
  */
-#define DEFAULT_SLEEP_WAKEUP_FREQ 99999999
+#define DEFAULT_SLEEP_WAKEUP_FREQ 800000
 static unsigned int sleep_wakeup_freq;
 
 /*
@@ -823,7 +823,7 @@ static int __init cpufreq_smartass2_init(void)
 	}
 
 	// Scale up is high priority
-	up_wq = create_rt_workqueue("ksmartass2_up");
+	up_wq = create_workqueue("ksmartass2_up");
 	down_wq = create_workqueue("ksmartass2_down");
 	if (!up_wq || !down_wq)
 		return -ENOMEM;
@@ -853,3 +853,4 @@ module_exit(cpufreq_smartass2_exit);
 MODULE_AUTHOR ("Erasmux");
 MODULE_DESCRIPTION ("'cpufreq_smartass2' - A smart cpufreq governor");
 MODULE_LICENSE ("GPL");
+
