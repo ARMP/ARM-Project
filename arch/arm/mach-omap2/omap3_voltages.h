18afa4c19d433c452e00b6f9db3fed646cbb30c3
Nicholas <nicholas.npcc@gmail.com>
2012-12-07 14:54:22 +0800

    Revert changes and added small fixes

--- arch/arm/mach-omap2/omap3_voltages.h
+++ arch/arm/mach-omap2/omap3_voltages.h
@@ -2,12 +2,11 @@
  * for MPU and CORE voltages
  */
 
-#define OMAP3630_VDD_MPU_OPP50_UV               980000
+#define OMAP3630_VDD_MPU_OPP50_UV               960000
 #define OMAP3630_VDD_MPU_OPP100_UV              1100000
 #define OMAP3630_VDD_MPU_OPP120_UV              1205000
-#define OMAP3630_VDD_MPU_OPP900_UV              1300000
-#define OMAP3630_VDD_MPU_OPP1G_UV               1325000
-#define OMAP3630_VDD_MPU_OPP12G_UV              1375000
+#define OMAP3630_VDD_MPU_OPP900_UV              1285000
+#define OMAP3630_VDD_MPU_OPP1G_UV               1300000
 
 #define OMAP3630_VDD_CORE_OPP50_UV              1000000
 #define OMAP3630_VDD_CORE_OPP100_UV             1200000
